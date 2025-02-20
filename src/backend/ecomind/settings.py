d94ac4c15bb557b6ea729f2f43e0190d21913014
brandistone <brandymabeya@gmail.com>
2025-02-21 01:06:51 +0300

    new pages

--- src/backend/ecomind/settings.py
+++ src/backend/ecomind/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',  # Django REST Framework
+    'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
