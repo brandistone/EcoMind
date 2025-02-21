fa49c7bf5174f29ea4edb689c44a1f6ab4753bfe
brandistone <brandymabeya@gmail.com>
2025-02-21 03:00:52 +0300

    changing importations

--- src/backend/api/models.py
+++ src/backend/api/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 
-class User(models.Model):
+class User(AbstractUser):
     username = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
 
