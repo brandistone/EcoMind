2a1e826a33046e293caff2206cc4a64919ff4b2e
brandistone <brandymabeya@gmail.com>
2025-02-21 03:25:23 +0300

    we wrap up for the day at 3:25 am

---
+++ src/backend/api/serializer.py
@@ -0,0 +1,18 @@
+from api.models import User, Profile
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['email'], validated_data['username'], validated_data['password'])
+        return user
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
